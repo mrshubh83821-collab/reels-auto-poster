4a2ac73148ada603eb24a4657be7c7b0b257e1ce
Shubh <mrshubh83821@gmail.com>
2026-08-24 16:15:47 +0530

    fix offset header

--- post-reel.js
+++ post-reel.js
@@ -130,6 +130,7 @@ async function uploadReelToFacebook(videoPath, description) {
     method: "POST",
     headers: {
       Authorization: `OAuth ${FB_PAGE_TOKEN}`,
+      offset: "0",
       "file_size": fileBuffer.length.toString(),
     },
     body: fileBuffer,
